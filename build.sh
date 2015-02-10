f977d6ba22437f091ff02d50d3974f90f66a8059
Bobby Allen <ballen@bobbyallen.me>
2015-02-10 15:57:50 +0000

    Added new build.sh script to automate the building of the module.

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+echo "########################################################"
+echo "# Sentora Module Compiler                              #"
+echo "# (c) Supared Limited, 2015                            #"
+echo "########################################################"
+echo ""
+echo "Checking required tools are installed..."
+type zip >/dev/null 2>&1 || { echo >&2 "The package 'zip' is required but not found on this server, please install and try again."; exit 1; }
+echo "Checking that a previously built module doesn't exist..."
+if [ -f "build/bouncer.zpp" ]; then
+    echo "  Previously built package has been found, cleaning up now..."
+    rm build/bouncer.zpp
+    echo "  Clean up complete!"
+fi
+
+echo "Compiling package..."
+cd src
+zip -r ../build/bouncer2.zpp *
+cd ../asd
+echo "Package compilation complete!
+echo "The package can be found in the build/ directory!"
+echo ""
+exit 0
