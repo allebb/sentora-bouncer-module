8f88a984a3a7670e5cbb91e7005209b3b4ad4939
Bobby Allen <ballen@bobbyallen.me>
2015-02-21 08:07:34 +0000

    Adding source code headers.

--- src/hooks/OnBoot.hook.php
+++ src/hooks/OnBoot.hook.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Bouncer - A SentoraCP module for only allowing or disallowing control panel login access to certain IP addresses. 
+ * @author Bobby Allen (ballen@bobbyallen.me)
+ * @copyright (c) 2015, Supared Limited
+ * @link https://github.com/supared/sentora-bouncer
+ * @license https://github.com/supared/sentora-bouncer/blob/master/LICENSE
+ * @version 1.0.0
+ */
 require_once 'kernel/Bouncer.php';
 $bouncer = new Bouncer(ctrl_options::GetSystemOption('bouncer_config'));
 $boucer->gaurd();
--- src/kernel/Bouncer.php
+++ src/kernel/Bouncer.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Bouncer - A SentoraCP module for only allowing or disallowing control panel login access to certain IP addresses. 
+ * @author Bobby Allen (ballen@bobbyallen.me)
+ * @copyright (c) 2015, Supared Limited
+ * @link https://github.com/supared/sentora-bouncer
+ * @license https://github.com/supared/sentora-bouncer/blob/master/LICENSE
+ * @version 1.0.0
+ */
 class Bouncer
 {
 
